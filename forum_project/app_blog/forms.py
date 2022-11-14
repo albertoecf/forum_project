4e7f71895a15d7624da79a858c8a14255b2c7cfd
aecf <81099969+albertoecf@users.noreply.github.com>
2022-11-14 09:06:30 +0100

    feat: forms

---
+++ forum_project/app_blog/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from app_blog.models import PostModel, CommentModel
+
+class PostFormModel(forms.ModelForm):
+
+    class Meta():
+        model = PostModel
+        fields = ('author','title','text')
+
+        widgets = {
+            'title' : forms.TextInput(attrs={'class':'textinputclass'}),
+            'text' : forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
+class CommentFormModel(forms.ModelForm):
+
+    class Meta():
+        model = CommentMode
+        fields = ('author','text')
+
+        widgets = {
+            'author' : forms.TextInput(attrs = {'class':'textinputclass'}),
+            'text' : forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
+
+        }
